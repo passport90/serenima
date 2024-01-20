88078755f53903e90a5e7b0c955495082f3efb49
Marhadiasha Kusumawardhana <marhadiasak@icloud.com>
2024-01-20 23:13:38 +0700

    feat: rename tsvector column

--- migrations/1705760034728-CreateFilmTable.ts
+++ migrations/1705760034728-CreateFilmTable.ts
@@ -8,20 +8,20 @@ export class CreateFilmTable1705760034728 implements MigrationInterface {
             imdb_id TEXT NOT NULL UNIQUE,
             title TEXT NOT NULL,
             release_date DATE,
-            name_vector tsvector,
+            title_vector tsvector,
             created_at TIMESTAMPTZ DEFAULT current_timestamp,
             updated_at TIMESTAMPTZ DEFAULT current_timestamp
         );
         
         CREATE INDEX idx_imdb_id ON film (imdb_id);
         CREATE INDEX idx_release_date ON film (release_date);
-        CREATE INDEX idx_name_vector ON film USING gin(name_vector);
+        CREATE INDEX idx_title_vector ON film USING gin(title_vector);
     `)
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(`
-        DROP INDEX IF EXISTS idx_name_vector;
+        DROP INDEX IF EXISTS idx_title_vector;
         DROP INDEX IF EXISTS idx_release_date;
         DROP INDEX IF EXISTS idx_imdb_id;
         
