6113f851656673d8b49b2220e3742d633fa58d14
Marhadiasha Kusumawardhana <marhadiasak@icloud.com>
2024-01-21 08:53:10 +0700

    build: remove prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -8,7 +8,6 @@ module.exports = {
   plugins: ['@typescript-eslint/eslint-plugin'],
   extends: [
     'plugin:@typescript-eslint/recommended',
-    'plugin:prettier/recommended',
   ],
   root: true,
   env: {
@@ -17,9 +16,6 @@ module.exports = {
   },
   ignorePatterns: ['**/*.cjs'],
   rules: {
-    '@typescript-eslint/interface-name-prefix': 'off',
-    '@typescript-eslint/explicit-function-return-type': 'off',
-    '@typescript-eslint/explicit-module-boundary-types': 'off',
-    '@typescript-eslint/no-explicit-any': 'off',
+
   },
 };
